33863a0ac294735d5840ee88cff177fbc5f57eaf
FOUDIL-BEY <nouri.foudil-bey@epitech.eu>
2025-10-08 17:14:12 +0200

    sdv

---
+++ users.js
@@ -0,0 +1,9 @@
+const express = require(express)
+const router = express.router()
+
+router.get("/", (req, res) =>{
+    res.status(200).json({message:"every users !"})
+})
+
+
+module.exports = router ;
