52795cdcc6eced02b1ba57b0db30986de45ca022
nouri3406 <nouri.foudil-bey@epitech.eu>
2025-10-10 10:59:09 +0200

    setup nouveau pc nouri

--- backend/index.js
+++ backend/index.js
@@ -6,23 +6,30 @@ const { Pool } = pkg;
 // Création de l'application Express
 const app = express();
 const port = 3000;
-
+ 
 // Middleware pour parser le JSON
 app.use(express.json());
-
+ 
 // Configuration de la connexion PostgreSQL
 const pool = new Pool({
   host: 'db',
   port: 5432,
   user: 'devuser',
   password: 'devpass',
-  database: 'devdb1'
+  database: 'devdb'
 });
+ 
+// Middleware pour parser le JSON
+app.use(express.json());
 
 // Vérification de la connexion à PostgreSQL
 pool.connect()
   .then(() => console.log('Connecté à PostgreSQL'))
   .catch(err => console.error('Erreur de connexion à PostgreSQL :', err));
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur mon API Node.js ! 🚀');
+});
 
 // Lancement du serveur
 app.listen(port, () => {
