3c256f708591b0b9cea8b9537ef5332e140e50ed
nouri3406 <nouri.foudil-bey@epitech.eu>
2025-10-10 15:17:43 +0200

    c augustin

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -2,7 +2,7 @@ const some_error = (err, req, res, next) => {
     console.log(err.stack());
     res.send(500).json({
         status:500,
-        message : "Oups a problem appeared "
+        message : "Oups a problem appeared ",
         error : err.message ,
         
     });
