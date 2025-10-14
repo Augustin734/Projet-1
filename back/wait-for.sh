26fa98199fb43b873fd2728d359ad1e99241054c
Augustin <augustin.viemont1@outlook.fr>
2025-10-14 10:43:45 +0200

    merge avec Nouri

--- back/wait-for.sh
+++ back/wait-for.sh
@@ -3,9 +3,7 @@ set -e
 host="$1"
 shift
 until nc -z "$host" 5432; do
-  echo "⏳ Waiting for Postgres at $host:5432..."
+  echo "Waiting for Postgres at $host:5432..."
   sleep 1
 done
 
-echo "✅ Postgres is up!"
-exec "$@"
