a21dba229097f4bac9bc090b89f60fa3f4f292bd
Augustin <augustin.viemont1@outlook.fr>
2025-10-17 11:01:53 +0200

    merge finale

--- site/API/Validator/candidatureValidate.js
+++
@@ -1,9 +0,0 @@
-import Joi from "Joi";
-
-export const candidatureSchema = Joi.object({
-    nom: Joi.string().required(),
-    lettre_motivation: Joi.string().required(),
-    date_de_postulation: Joi.date().required(),
-    status_candidature: Joi.string().required(),
-    //status_candidature: Joi.string().valid("acceptée", "refusée", "en attente").required(),
-});
